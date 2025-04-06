bb677400ba09da1498cc9c525fb01790a5675830
Ammar Ali <ammaralialiwa@gmail.com>
2025-04-06 10:14:17 -0600

    how the route works

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +11,8 @@ export default function Index() {
       "
     >
       <Text className=" text-primary font-extrablod text-5xl">Welcomew fd</Text>
+       <Link href="/onboarding">onboarding</Link>
+       <Link href="/movie/avangers">Avenger Movie</Link>
     </View>
   );
 }
