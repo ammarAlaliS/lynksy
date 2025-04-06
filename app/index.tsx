8ffaba63887f30014c5a112383efa43943489622
Ammar Ali <ammaralialiwa@gmail.com>
2025-04-06 09:58:03 -0600

    set up tailwind css config

--- app/index.tsx
+++ app/index.tsx
@@ -3,9 +3,13 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      className=" flex-1 justify-center items-center"
+      className="
+        flex-1 
+        justify-center 
+        items-center
+      "
     >
-      <Text className=" text-red-800 font-extrablod text-5xl">Welcomew fd</Text>
+      <Text className=" text-primary font-extrablod text-5xl">Welcomew fd</Text>
     </View>
   );
 }
