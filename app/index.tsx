0a036e17cffc0f2ded7d261a51d07914ccd3f464
Ammar Ali <ammaralialiwa@gmail.com>
2025-04-05 22:28:12 -0600

    fix: tailwind css error

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,9 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className=" flex-1 justify-center items-center"
     >
-      <Text>Welcomew</Text>
+      <Text className=" text-red-800 font-extrablod text-5xl">Welcomew fd</Text>
     </View>
   );
 }
