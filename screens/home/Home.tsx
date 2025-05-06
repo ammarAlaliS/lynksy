d9657eec6f159ac7d85cfdbe474bd9e5e8b3b24f
Ammar Ali <ammaralialiwa@gmail.com>
2025-05-05 21:53:55 -0600

    clean code

--- screens/home/Home.tsx
+++ screens/home/Home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import React, { useRef } from 'react';
+import { ScrollView, View, Text, Animated } from 'react-native';
 import { useGlobalStyles } from "../global_screen_styles/Style_global";
 import StoriesViewer from "@/components/Stories/StoriesViewer";
 import RifaCard from "@/components/rifles/RiflesCard";
@@ -10,68 +10,83 @@ import GeneralTitle from '@/components/General_title';
 import Icons from '@/components/Icon';
 import { Icon } from 'react-native-elements';
 import { ejemploRifas } from '@/constants/Rifles';
-
+import { useHeaderAnimation } from '@/context/HeaderAnimationContext';
 
 const Home = ({ setSwipeEnabled }: any) => {
   const GlobalStyles = useGlobalStyles();
   const themeColors = useSelector(selectTheme);
-
+  
+  // Definimos la animación (aunque no esté animándose aún)
+      const { headerTranslateY } = useHeaderAnimation();
   return (
-    <View style={GlobalStyles.container}>
-      <ScrollView>
-      <StoriesViewer />
-      <View style={{
+    <Animated.View
+      style={{
+        transform: [{ translateY: headerTranslateY }],
+        flex:1,
         backgroundColor: themeColors.background,
-        marginTop: 4,
-        marginBottom:3,
-        display: 'flex',
-        justifyContent: 'center',
-        gap:10
-      }}>
+        zIndex: 10,
+        position: 'absolute',
+        top: 50,
+        left: 0,
+        right: 0, 
+        bottom:0,
+        flexDirection: 'column',
+        borderBottomWidth: 1,
+        borderColor: themeColors.border,
+        paddingVertical: 8,
+      }}
+    >
+      <ScrollView>
+        <StoriesViewer />
         <View style={{
-          paddingVertical: 10,
-          borderBottomWidth: 1,
-          borderColor: themeColors.border,
-          backgroundColor: themeColors.backgroundSection,
-          paddingHorizontal:10
+
+          marginTop: 4,
+          marginBottom: 3,
+          display: 'flex',
+          justifyContent: 'center',
+          gap: 10,
           
         }}>
-          <GeneralTitle
-            icon={
-              <Icon
-                name="spray-can"
-                type="font-awesome-5"
-                color={themeColors.white}
-              />
-            }
-            title={'Perfumes'} themeColors={themeColors}
-            icon_two={
-              <Icons
-                onPress={() => null}
-                style={{
-                  backgroundColor: themeColors.green,
-                  width: 32,
-                  height: 32,
-                  borderRadius: 9999,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  position: 'relative',
-                }}
-                icon={<Icon name="chevron-right" color={themeColors.background} />}
-              />
-            }
-          />
+          <View style={{
+            paddingVertical: 10,
+            borderBottomWidth: 1,
+            borderColor: themeColors.border,
+            backgroundColor: themeColors.backgroundSection,
+            paddingHorizontal: 10
+          }}>
+            <GeneralTitle
+              icon={
+                <Icon
+                  name="spray-can"
+                  type="font-awesome-5"
+                  color={themeColors.white}
+                />
+              }
+              title={'Perfumes'}
+              themeColors={themeColors}
+              icon_two={
+                <Icons
+                  onPress={() => null}
+                  style={{
+                    backgroundColor: themeColors.green,
+                    width: 32,
+                    height: 32,
+                    borderRadius: 9999,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    position: 'relative',
+                  }}
+                  icon={<Icon name="chevron-right" color={themeColors.background} />}
+                />
+              }
+            />
+          </View>
+          <View style={{ paddingBottom: 10 }}>
+            <RifaCard ejemploRifas={ejemploRifas} />
+          </View>
         </View>
-        <View style={{
-          paddingBottom:10,
-        }}>
-       
-          <RifaCard ejemploRifas={ejemploRifas}  />
-  
-        </View>
-      </View>
       </ScrollView>
-    </View>
+    </Animated.View>
   );
 };
 
